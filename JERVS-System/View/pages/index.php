ff2de3f81de265a71ec19d2a86ba33bb6236bea7
Georgy <georgedavies.arellano@gmail.com>
2025-06-07 16:24:39 +0800

    log-in design is okay - Georgy

--- JERVS-System/View/pages/index.php
+++ JERVS-System/View/pages/index.php
@@ -7,50 +7,58 @@ if (isset($_SESSION['admin'])) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>JERVS SPORTWEAR Login</title>
-  <link rel="stylesheet" href="../assets/stylesheet/index.css">
-</head>
-
-<body>
-  <div class="login-container">
-    <div class="login-card">
-      <div class="login-left">
-        <div class="logo-top">
-          <img src="logo.png" alt="Logo" />
-        </div>
-        <h2>JERVS SPORTWEAR</h2>
-        <p class="subtitle">Start Your Own Brand!</p>
-
-        <form class="login-form" id="loginForm">
-          <label for="username">Username</label>
-          <input type="text" id="username" name="username" placeholder="Enter your username">
-
-          <label for="password">Password</label>
-          <div class="password-wrapper">
-            <input type="password" id="password" name="password" placeholder="********">
-            <span class="toggle-password">👁️</span>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/stylesheet/index.css" />
+    <link rel="stylesheet" href="../assets/stylesheet/globals.css" />
+  </head>
+  <body>
+    <div class="box">
+      <div class="log-in">
+        <div class="overlap">
+          <div class="div">
+            <img class="logo" src="../assets/img/logo-1.png" />
+            <div class="form-container">
+              <div class="headline">
+                <div class="overlap-3">
+                  <h1 class="JERVS-sportwear">JERVS SPORTWEAR</h1>
+                  <p class="text-wrapper-6">Start Your Own Brand!</p>
+                </div>
+              </div>
+              
+              <div class="email-form">
+                <label class="text-wrapper-3">Username</label>
+                <div class="div-wrapper">
+                  <input type="text" class="text-wrapper-2" placeholder="Enter your username" />
+                </div>
+              </div>
+              
+              <div class="password-form">
+                <label class="text-wrapper-5">Password</label>
+                <div class="overlap-2">
+                  <input type="password" class="text-wrapper-4" placeholder="**********" />
+                  <img class="eye-off" src="../assets/img/eye-off.svg" alt="Toggle password visibility" />
+                </div>
+              </div>
+              
+              <div class="overlap-group-wrapper">
+                <button class="overlap-group">
+                  <span class="text-wrapper">Log in</span>
+                </button>
+              </div>
+            </div>
+            
+            <img class="right-side" src="../assets/img/right-side.png" alt="Decorative background" />
+            <img class="img" src="../assets/img/logo-2.png" />
           </div>
-
-          <button type="submit" class="login-btn">Log In</button>
-          <div id="message"></div>
-        </form>
-      </div>
-
-
-      <div class="login-right">
-        <img src="basketball.png" alt="Basketball Player" />
-        <div class="logo-bottom">
-          <img src="logo.png" alt="Logo" />
         </div>
       </div>
     </div>
-  </div>
+  </body>
   <script src="../script/index.js"></script>
-</body>
-
 </html>
