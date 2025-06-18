5d59c79bd137e1f0d9c80b3821a27591e12ffc74
Kurt <trythisnet03@gmail.com>
2025-06-18 18:39:49 +0800

    - logout fix

--- Factory-Modules/Controller/logout.php
+++ Factory-Modules/Controller/logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     
-    unset($_SESSION['member_id']);
+    unset($_SESSION['member']);
 
     echo '<script> alert("Logging Out"); </script>';
     header('Location: ../view/auth/index.php');
--- JERVS-System/Controller/logout.php
+++ JERVS-System/Controller/logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     
-    unset($_SESSION['admin_id']);
+    unset($_SESSION['admin']);
     echo '<script> alert("Logging Out"); </script>';
 
     header('Location: ../View/pages/index.php');
